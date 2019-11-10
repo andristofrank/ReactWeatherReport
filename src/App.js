b0117015dc546af919466486d28191cca562579f
Andrei Tofan <56041464+andristofrank@users.noreply.github.com>
2019-11-10 15:17:15 +0100

    pure basic frontend changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import './App.css';
-
 function App() {
+
   return (
     <div>
     <div className="App">
-   Please Choose a city
+      <h2>Please Choose a city</h2>
+      <SelectBox/>
+     <DatePicker/>
+     <TimePicker/>
+     <DatePicker/>
+     <TimePicker/>
+     <button className="btn btn-success">Show Data</button>
     </div>
-    <SelectBox/>
+
     </div>
   );
 };
 
 const SelectBox = ()=>(
-    <select value="1">
+    <select class="form-control w-70" value="1">
       <option value="1">Horsens</option>
       <option value="2">Aarhus</option>
       <option value="3">Copenhagen</option>
